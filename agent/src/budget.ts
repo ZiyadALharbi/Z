22762c5237cb335c2748fe21fbba0e732a736496
ZiyadALharbi <ziyad.swengineer@gmail.com>
2026-04-25 02:18:43 +0300

    added simple Iteration Budget Module

---
+++ agent/src/budget.ts
@@ -0,0 +1,21 @@
+export class IterationBudget {
+    private remaining: number;
+
+    constructor(maxIterations: number){
+        if (maxIterations < 1) {
+            throw new Error('maxIterations must be at least 1');
+        }
+        this.remaining = maxIterations
+    }
+
+    consume(): boolean {
+        if (this.remaining <= 0 ) return false;
+        this.remaining -= 1;
+        return true;
+    }
+
+    getRemaining(): number {
+        return this.remaining;
+    }
+
+}
